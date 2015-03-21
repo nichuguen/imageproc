88f86a9405f18d4fdba399e5a26771c29e0a8b2c
gary.mariglia <gary.marigliano@he-arc.ch>
2015-03-21 09:54:55 +0100

    attributes in ImageField are no longer text but opencv img objects

--- ImageField.py
+++ ImageField.py
@@ -1,9 +1,19 @@
+import cv2
+
 class ImageField:
-	def __init__(self, name, realImg, maskImg, text):
+	def __init__(self, name, realImgPath, maskImgPath, textPath):
 		self._name = name
-		self._realImg = realImg
-		self._maskImg = maskImg
-		self._text = text
+		self._realImg = cv2.imread(realImgPath)
+		self._maskImg = cv2.imread(maskImgPath)
+		self._textPath = textPath
 
 	def __repr__(self):
 		return self._name
+
+	@property
+	def realImg(self):
+		return self._realImg
+
+	@property
+	def maskImg(self):
+		return self._maskImg
