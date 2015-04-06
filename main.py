7fbcb7a648a394f8f1f192d150b15212439c4da3
gary.mariglia <gary.marigliano@he-arc.ch>
2015-04-06 14:23:59 +0200

    commit de fou

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from OpenCVTools import OpenCVTools
 from ImageField import ImageField
 
 HOUGH_OFFSET = 6000
-
+MIN_MERGED_LINES = 3
+THRESHOLD = 0.9
+CANNY_THRESHOLD = 40
 
 def isSameTheta(listClasses, rt, threshold, index):
 	mean = 0
@@ -26,7 +28,6 @@ def sortIndex(listRhoTheta, threshold, index):
 	else:
 		raise
 
-
 def computeMean(truc2):
 	t = 0
 	r = 0
@@ -37,79 +38,6 @@ def computeMean(truc2):
 	if length > 0:
 		return (r / length, t/length)
 	return (0,0)
-
-
-def main():
-	imageField = ImageField("6872195.0", r'data\denens062014_50\6872195.0.jpg', r'data\denens062014_50\6872195.0_mask.jpg', r'data\denens062014_50\6872195.0.txt')
-	# imageField = ImageField("6870221.0", r'data\denens062014_50\6870221.0.jpg', r'data\denens062014_50\6870221.0_mask.jpg', r'data\denens062014_50\6870221.0.txt')
-	img = imageField.realImg
-
-	grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	OpenCVTools.showWindow("grey", grey)
-
-	#grey = cv2.GaussianBlur(grey, (3,3), 0)
-	grey = cv2.GaussianBlur(grey, (9,9), 0)
-	# grey = cv2.bilateralFilter(grey, 4, 120,120)
-
-	OpenCVTools.showWindow("bilateralfilter", grey)
-	edges = cv2.Canny(grey,40,3*40)
-	canny = edges
-	OpenCVTools.showWindow("canny", edges)
-
-	# th2 = cv2.adaptiveThreshold(grey,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,2)
-	# OpenCVTools.showWindow("th2", th2)
-
-	kernel = np.ones((10,10),np.uint8)
-	#edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel, iterations=2)
-	edges = cv2.dilate(edges, kernel, iterations=3)
-	OpenCVTools.showWindow("dilate", edges)
-
-	kernel = np.ones((12,12), np.uint8)
-	edges = cv2.erode(edges, kernel, iterations=2)
-	OpenCVTools.showWindow("erode", edges)
-
-	edges = cv2.Canny(edges,40,3*40)
-	OpenCVTools.showWindow("canny 2", edges)
-
-
-
-	lines = cv2.HoughLines(edges,1,np.pi/180,100)
-
-	#lines = sorted(lines, key=lambda x : getPoints(x.rho, x.theta)[0])[:10]
-
-	listRhoTheta = []
-	for line in lines:
-		for rho,theta in line:
-			listRhoTheta.append((rho, theta))
-			(norm, x1, y1, x2, y2) = getPoints(rho, theta)
-			#cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-	listRhoTheta.sort(key= lambda x: x[0])
-
-	threshold = 0.5
-	sortedTheta = sortIndex(listRhoTheta, threshold, 1)
-	listMalentendu = []
-	for listTheta in sortedTheta:
-		listMalentendu.append(sortIndex(listTheta, 100, 0))
-
-	#listMalentendu = [x for x in listMalentendu if len(x) > 2]
-
-	listMeans = []
-	for truc in listMalentendu:
-		for truc2 in truc:
-			if len(truc2) > 10:
-				listMeans.append(computeMean(truc2))
-			print(len(truc2))
-
-	#print(listMeans)
-
-
-	for mean in listMeans:
-		(norm, x1, y1, x2, y2) = getPoints(mean[0], mean[1])
-		cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-	OpenCVTools.showWindow(imageField.name, img)
-	cv2.waitKey(0)
 
 def getPoints(rho, theta):
 	a = np.cos(theta)
@@ -123,6 +51,64 @@ def getPoints(rho, theta):
 
 	norm = (x1* x2)**2 + (y1* y2)**2
 	return (norm, x1, y1, x2, y2)
+
+
+def main():
+	#imageField = ImageField("6872195.0", r'data\denens062014_50\6872195.0.jpg', r'data\denens062014_50\6872195.0_mask.jpg', r'data\denens062014_50\6872195.0.txt')
+	imageField = ImageField("6870221.0", r'data\denens062014_50\6870221.0.jpg', r'data\denens062014_50\6870221.0_mask.jpg', r'data\denens062014_50\6870221.0.txt')
+
+	#imageField = ImageField("6872195.0", r'data\denensprof062014_50\6872195.0.jpg', r'data\denensprof062014_50\6872195.0_mask.jpg', r'data\denensprof062014_50\6872195.0.txt')
+	# imageField = ImageField("6870221.0", r'data\denensprof062014_50\6870221.0.jpg', r'data\denensprof062014_50\6870221.0_mask.jpg', r'data\denensprof062014_50\6870221.0.txt')
+	img = imageField.realImg
+
+	grey = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	OpenCVTools.showWindow("grey", grey)
+
+	grey = cv2.GaussianBlur(grey, (9,9), 0)
+	# grey = cv2.bilateralFilter(grey, 4, 120,120)
+
+	OpenCVTools.showWindow("gaussian filter", grey)
+	edges = cv2.Canny(grey,CANNY_THRESHOLD,3*CANNY_THRESHOLD)
+	OpenCVTools.showWindow("canny", edges)
+
+	kernel = np.ones((10,10),np.uint8)
+	edges = cv2.dilate(edges, kernel, iterations=3)
+	OpenCVTools.showWindow("dilate", edges)
+
+	kernel = np.ones((12,12), np.uint8)
+	edges = cv2.erode(edges, kernel, iterations=2)
+	OpenCVTools.showWindow("erode", edges)
+
+	edges = cv2.Canny(edges,40,3*40)
+	OpenCVTools.showWindow("canny 2", edges)
+
+	lines = cv2.HoughLines(edges,1,np.pi/180,120)
+
+	listRhoTheta = []
+	for line in lines:
+		for rho,theta in line:
+			listRhoTheta.append((rho, theta))
+			(norm, x1, y1, x2, y2) = getPoints(rho, theta)
+
+	listRhoTheta.sort(key= lambda x: x[0])
+
+	sortedTheta = sortIndex(listRhoTheta, THRESHOLD, 1)
+	listMalentendu = []
+	for listTheta in sortedTheta:
+		listMalentendu.append(sortIndex(listTheta, 100, 0))
+
+	listMeans = []
+	for truc in listMalentendu:
+		for truc2 in truc:
+			if len(truc2) > MIN_MERGED_LINES:
+				listMeans.append(computeMean(truc2))
+
+	for mean in listMeans:
+		(norm, x1, y1, x2, y2) = getPoints(mean[0], mean[1])
+		cv2.line(img,(x1,y1),(x2,y2),(0,0,255),8)
+
+	OpenCVTools.showWindow(imageField.name, img)
+	cv2.waitKey(0)
 
 if __name__ == '__main__':
 	main()
